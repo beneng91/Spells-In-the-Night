f6a5f8494f229992a3baec44eb336a8f54a542b0
beneng91 <103171571+beneng91@users.noreply.github.com>
2024-04-11 10:50:42 +1000

    initial

--- Source/SpellsInTheNight/SpellsInTheNight.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "SpellsInTheNight.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SpellsInTheNight, "SpellsInTheNight" );
- 
